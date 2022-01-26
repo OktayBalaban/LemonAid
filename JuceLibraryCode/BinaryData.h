c8c171e396edddbafd118504afdee22437122c31
Oktay <47466287+OktayBalaban@users.noreply.github.com>
2022-01-26 09:25:45 +0300

    Merging Agnes's work and Oktay's Work

---
+++ JuceLibraryCode/BinaryData.h
@@ -0,0 +1,57 @@
+/* =========================================================================================
+
+   This is an auto-generated file: Any edits you make may be overwritten!
+
+*/
+
+#pragma once
+
+namespace BinaryData
+{
+    extern const char*   DailyTrack0_csv;
+    const int            DailyTrack0_csvSize = 0;
+
+    extern const char*   DailyTrack1_csv;
+    const int            DailyTrack1_csvSize = 0;
+
+    extern const char*   DailyTrack2_csv;
+    const int            DailyTrack2_csvSize = 0;
+
+    extern const char*   DailyTrack4_csv;
+    const int            DailyTrack4_csvSize = 0;
+
+    extern const char*   Goals0_csv;
+    const int            Goals0_csvSize = 0;
+
+    extern const char*   Goals1_csv;
+    const int            Goals1_csvSize = 0;
+
+    extern const char*   Goals2_csv;
+    const int            Goals2_csvSize = 0;
+
+    extern const char*   Goals4_csv;
+    const int            Goals4_csvSize = 0;
+
+    extern const char*   Habits_csv;
+    const int            Habits_csvSize = 88;
+
+    extern const char*   Habits_csv2;
+    const int            Habits_csv2Size = 0;
+
+    // Number of elements in the namedResourceList and originalFileNames arrays.
+    const int namedResourceListSize = 10;
+
+    // Points to the start of a list of resource names.
+    extern const char* namedResourceList[];
+
+    // Points to the start of a list of resource filenames.
+    extern const char* originalFilenames[];
+
+    // If you provide the name of one of the binary resource variables above, this function will
+    // return the corresponding data and its size (or a null pointer if the name isn't found).
+    const char* getNamedResource (const char* resourceNameUTF8, int& dataSizeInBytes);
+
+    // If you provide the name of one of the binary resource variables above, this function will
+    // return the corresponding original, non-mangled filename (or a null pointer if the name isn't found).
+    const char* getNamedResourceOriginalFilename (const char* resourceNameUTF8);
+}
