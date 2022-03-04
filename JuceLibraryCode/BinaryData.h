7f605fabdd3a365c3bfd31767c3c88c4180f7858
YL <lyp.space@outlook.com>
2022-03-04 11:13:05 +0000

    Added Resources folder. put it in your target folder.

--- JuceLibraryCode/BinaryData.h
+++ JuceLibraryCode/BinaryData.h
@@ -8,6 +8,21 @@
 
 namespace BinaryData
 {
+    extern const char*   _4_stages_of_a_habit_png;
+    const int            _4_stages_of_a_habit_pngSize = 2483299;
+
+    extern const char*   form_a_habit_PNG;
+    const int            form_a_habit_PNGSize = 1889067;
+
+    extern const char*   habit_loop_png;
+    const int            habit_loop_pngSize = 1814712;
+
+    extern const char*   Resources_csv;
+    const int            Resources_csvSize = 1616;
+
+    extern const char*   what_is_a_habit_png;
+    const int            what_is_a_habit_pngSize = 2393732;
+
     extern const char*   Habits_csv;
     const int            Habits_csvSize = 88;
 
@@ -21,7 +36,7 @@ namespace BinaryData
     const int            Banner_pngSize = 675350;
 
     // Number of elements in the namedResourceList and originalFileNames arrays.
-    const int namedResourceListSize = 4;
+    const int namedResourceListSize = 9;
 
     // Points to the start of a list of resource names.
     extern const char* namedResourceList[];
