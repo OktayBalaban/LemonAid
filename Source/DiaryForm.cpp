1caa96ff22133d32b560d631b3f804fd6fa33f3b
YL <lyp.space@outlook.com>
2022-03-09 09:56:07 +0000

    merge Agnes's DiaryForm fix by hand.

--- Source/DiaryForm.cpp
+++ Source/DiaryForm.cpp
@@ -180,6 +180,16 @@ void DiaryForm::buttonClicked (juce::Button* buttonThatWasClicked)
     if (buttonThatWasClicked == juce__textButton1.get())
     {
         //[UserButtonCode_juce__textButton1] -- add your button handler code here..
+        juce::String oldEntry = juce__textEditor3->getText();
+        juce::String newEntry = juce__textEditor2->getText();
+        juce::String dateOfEntry = juce__textEditor->getText();
+        juce::String newLine = "\n";
+
+        std::string fullText = (oldEntry + "\n" + dateOfEntry + "\n" + newEntry + "\n").toStdString();
+        // Show the text
+        juce__textEditor3->setText(fullText, juce::NotificationType::dontSendNotification);
+
+        updateDiary(fullText);
         //[/UserButtonCode_juce__textButton1]
     }
     else if (buttonThatWasClicked == juce__textButton2.get())
